162d5283826806d4cde6ee51d301315d372bb6ee
mononoken <ken.maeshima@pm.me>
2022-01-17 07:50:36 -0600

    Create current player and set_current_player method in Game.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -31,6 +31,16 @@ module Messagable
     'Welcome to a game of Tic-tac-toe!'
   end
 
+  def announce_players_msg(first_player, second_player)
+    "#{first_player.name} is assigned #{first_player.mark}."
+    "#{second_player.name} is assigned #{second_player.mark}."
+  end
+
+  def instruction_msg
+    'Each player will take turns choosing grid coordinates to place their marks.'
+    "Grid coordinates must be formatted similar to these examples: 'a1' 'b2'."
+  end
+
   def first_round_msg(first_player)
     "#{first_player.name} starts. Please input a grid coordinate:"
   end
@@ -77,13 +87,14 @@ class Player
     @mark = mark
   end
 
-  def get_choice
+  def set_choice
     @choice = gets.chomp
   end
 end
 
 class Game
   include EndCondition
+  include Messagable
   attr_reader :player1, :player2
 
   def initialize
@@ -92,11 +103,24 @@ class Game
     @grid = Grid.new
   end
 
-  def run_round
+  def set_current_player
+    if @current_player == self.player1
+      @current_player = self.player2
+    else
+      @curent_player = self.player1
+  end
 
+  def run_round
+    set_current_player
+    self.grid.display_grid
+    puts prompt_choice_msg
+    self.current_player.set_choice
   end
 
   def run_game
+    puts welcome_msg
+    puts instruction_msg
+    puts announce_players_msg
     run_round until EndCondition.win? || EndCondition.draw?
   end
 end
