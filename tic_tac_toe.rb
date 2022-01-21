b8fae008399ef8c66a589213b60c983fc0bb9e2f
mononoken <ken.maeshima@pm.me>
2022-01-20 20:08:09 -0600

    Change Board instance to board to distinguish from grid hash.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,22 +2,22 @@ require './lib/player'
 require './lib/board'
 
 module EndCondition
-  def win?(player, grid)
-    if grid[:a1] == player.mark && grid[:a2] == player.mark && grid[:a3] == player.mark
+  def win?(player, board)
+    if board[:a1] == player.mark && board[:a2] == player.mark && board[:a3] == player.mark
       true
-    elsif grid[:b1] == player.mark && grid[:b2] == player.mark && grid[:b3] == player.mark
+    elsif board[:b1] == player.mark && board[:b2] == player.mark && board[:b3] == player.mark
       true
-    elsif grid[:c1] == player.mark && grid[:c2] == player.mark && grid[:c3] == player.mark
+    elsif board[:c1] == player.mark && board[:c2] == player.mark && board[:c3] == player.mark
       true
-    elsif grid[:a1] == player.mark && grid[:b1] == player.mark && grid[:c1] == player.mark
+    elsif board[:a1] == player.mark && board[:b1] == player.mark && board[:c1] == player.mark
       true
-    elsif grid[:a2] == player.mark && grid[:b2] == player.mark && grid[:c2] == player.mark
+    elsif board[:a2] == player.mark && board[:b2] == player.mark && board[:c2] == player.mark
       true
-    elsif grid[:a3] == player.mark && grid[:b3] == player.mark && grid[:c3] == player.mark
+    elsif board[:a3] == player.mark && board[:b3] == player.mark && board[:c3] == player.mark
       true
-    elsif grid[:a1] == player.mark && grid[:b2] == player.mark && grid[:c3] == player.mark
+    elsif board[:a1] == player.mark && board[:b2] == player.mark && board[:c3] == player.mark
       true
-    elsif grid[:a3] == player.mark && grid[:b2] == player.mark && grid[:c1] == player.mark
+    elsif board[:a3] == player.mark && board[:b2] == player.mark && board[:c1] == player.mark
       true
     else
       false
@@ -43,13 +43,13 @@ module Messagable
 
   def instruction_msg
     [
-      'Each player will take turns choosing grid coordinates to place their marks.',
-      "Grid coordinate inputs must be formatted similar to these examples: 'a1' 'b2'."
+      'Each player will take turns choosing board coordinates to place their marks.',
+      "board coordinate inputs must be formatted similar to these examples: 'a1' 'b2'."
     ]
   end
 
   def prompt_choice_msg(player)
-    "#{player.name}'s turn. Please input a grid coordinate:"
+    "#{player.name}'s turn. Please input a board coordinate:"
   end
 
   def announce_winner(winner)
@@ -65,12 +65,12 @@ class Game
   include EndCondition
   include Messagable
   attr_reader :player1, :player2
-  attr_accessor :grid, :current_player
+  attr_accessor :board, :current_player
 
   def initialize
     @player1 = Player.new('Player 1', 'X')
     @player2 = Player.new('Player 2', 'O')
-    @grid = Board.new
+    @board = Board.new
     @current_player = nil
   end
 
@@ -91,23 +91,23 @@ class Game
 
   def run_round
     set_current_player
-    self.grid.display_board
+    self.board.display_board
     puts prompt_choice_msg(self.current_player)
     self.current_player.set_choice
-    self.grid.mark_choice(self.current_player, self.current_player.choice)
+    self.board.mark_choice(self.current_player, self.current_player.choice)
   end
 
   def run_game
     intro
-    run_round until self.win?(self.current_player, self.grid.grid) || self.draw?(self.grid.grid)
-    self.grid.display_board
-    if self.win?(self.player1, self.grid.grid)
+    run_round until self.win?(self.current_player, self.board.grid) || self.draw?(self.board.grid)
+    self.board.display_board
+    if self.win?(self.player1, self.board.grid)
       winner = self.player1
       puts announce_winner(winner)
-    elsif self.win?(self.player2, self.grid.grid)
+    elsif self.win?(self.player2, self.board.grid)
       winner = self.player2
       puts announce_winner(winner.name)
-    elsif self.draw?(self.grid.grid)
+    elsif self.draw?(self.board.grid)
       puts draw_msg
     else
       puts 'What happened?'
