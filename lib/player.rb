927fa25ab23f095f3f3703394464b0144d0e8490
mononoken <ken.maeshima@pm.me>
2022-01-22 08:59:54 -0600

    Decided to not make Choice class. Add valid_choice? to Player.

--- lib/player.rb
+++ lib/player.rb
@@ -11,4 +11,9 @@ class Player
   def set_choice
     self.choice = gets.chomp.downcase
   end
+
+  def valid_choice?(choice)
+    valid_choices = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
+    valid_choices.include?(choice)
+  end
 end
