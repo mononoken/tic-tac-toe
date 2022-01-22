0645dae20ea986c34e23e1689a7a6ce0a54a2f61
mononoken <ken.maeshima@pm.me>
2022-01-22 09:13:35 -0600

    Add @@valid_choices to Player to track which grid spots on the board have been taken.

--- lib/player.rb
+++ lib/player.rb
@@ -2,18 +2,24 @@ class Player
   attr_reader :name, :mark
   attr_accessor :choice
 
+  @@valid_choices = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
+
   def initialize(name, mark)
     @name = name
     @mark = mark
     @choice = nil
   end
 
-  def set_choice
-    self.choice = gets.chomp.downcase
+  def valid_choice?(choice)
+    @@valid_choices.include?(choice)
   end
 
-  def valid_choice?(choice)
-    valid_choices = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
-    valid_choices.include?(choice)
+  def set_choice
+    self.choice = gets.chomp.downcase
+    if valid_choice?(self.choice)
+      @@valid_choices.delete(self.choice)
+    else
+      self.choice = nil
+    end
   end
 end
