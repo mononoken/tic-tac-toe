1ca0ccc9f3b9ef0a386e774029c7bac6bb8e9591
mononoken <ken.maeshima@pm.me>
2022-08-03 11:46:55 -0700

    Begin player_spec.rb

---
+++ spec/player_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../lib/player'
+
+describe Player do
+  subject(:player_choosing) { described_class.new }
+
+  describe '#valid_choice?' do
+
+
+    it 'when choice is valid' do
+    end
+
+    it 'when choice is nil' do
+    end
+  end
+
+  describe '#set_choice' do
+    it 'when blanks' do
+    end
+  end
+end
