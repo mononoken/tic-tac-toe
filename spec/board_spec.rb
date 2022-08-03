17a9a625dbfe25b404968d1ea5b846d59726be2b
mononoken <ken.maeshima@pm.me>
2022-08-03 12:36:12 -0700

    Begin board_spec.rb

---
+++ spec/board_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board'
+
+describe Board do
+  subject(:something) { described_class.new }
+
+  describe '#method' do
+    it 'when something' do
+    end
+  end
+end
